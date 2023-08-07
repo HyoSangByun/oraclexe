80306269eb8f4ab38c125fbe6f9406e682519886
HyoSangByun <48824656+HyoSangByun@users.noreply.github.com>
2023-08-07 11:26:38 +0900

    Update ex07-select-join.sql

--- ex07-select-join.sql
+++ ex07-select-join.sql
@@ -55,3 +55,108 @@ ON e.department_id = d.department_id
 JOIN locations l
 ON d.location_id = l.location_id;
 
+-- 조인에 추가 조건 적용
+SELECT e.employee_id, e.last_name, e.department_id,
+    d.department_id, d.location_id
+FROM employees e JOIN departments d
+ON e.department_id = d.department_id
+AND e.manager_id = 149;
+
+/*
+테이블 자체 조인
+    on 절을 사용하는 Self Join
+*/
+SELECT worker.last_name emp, manager.last_name mgr
+FROM employees worker JOIN employees manager
+ON worker.manager_id = manager.employee_id;
+
+/*
+Nonequijoin
+    등호 연산자 외의 다른 연산자를 포함하는 조인 조건입니다.
+    
+CREATE TABLE job_Grades (
+grade_level CHAR(1),
+lowest_sal NUMBER(8,2) NOT NULL,
+highest_sal NUMBER(8,2) NOT NULL
+);
+
+ALTER TABLE job_grades
+ADD CONSTRAINT jobgrades_grade_pk PRIMARY KEY (grade_level);
+
+INSERT INTO job_grades VALUES ('A', 1000, 2999);
+INSERT INTO job_grades VALUES ('B', 3000, 5999);
+INSERT INTO job_grades VALUES ('C', 6000, 9999);
+INSERT INTO job_grades VALUES ('D', 10000, 14999);
+INSERT INTO job_grades VALUES ('E', 15000, 24999);
+INSERT INTO job_grades VALUES ('F', 25000, 40000);
+
+COMMIT;
+
+*/
+
+SELECT e.last_name, e.salary, j.grade_level
+FROM employees e JOIN job_grades j
+ON e.salary BETWEEN j.lowest_sal AND j.highest_sal;
+-- 등호가 아니라도 범위로도 조인 할 수 있다.
+
+/*
+INNER JOIN과 OUTER JOIN
+
+    INEER JOIN
+        일치하지 않는 행은 출력에 표시되지 않습니다. (교집합 해당 행 출력)
+    OUTER JOIN
+        한 테이블의 행을 기반으로 다른 테이블과의 연결이 없는 행까지 포함하여 반환합니다.
+*/
+
+/*
+LEFT OUTER JOIN
+*/
+
+SELECT e.last_name, e.department_id, d.department_name
+FROM employees e
+LEFT OUTER JOIN departments d
+ON e.department_id = d.department_id;
+-- departments 테이블에 대응되는 행이 없어도
+-- 왼쪽 테이블인 employees 테이블의 모든 행을 검색합니다.
+
+--위 쿼리와 동일
+SELECT e.last_name, e.department_id, d.department_name
+FROM employees e, departments d
+WHERE e.department_id = d.department_id(+); --LEFT OUTER JOIN
+
+/*
+RIGHT OUTER JOIN
+    employees 테이블에 대응되는 행이 없어도
+    오른쪽 테이블인 departments 테이블의 모든 행을 검색합니다.
+*/
+SELECT e.last_name, d.department_id, d.department_name
+FROM employees e
+RIGHT OUTER JOIN departments d
+ON e.department_id = d.department_id;
+
+/*
+FULL OUTER JOIN
+    DEPARTMENTS, EMPLOYEES 대응되는 행이 없어도
+    테이블의 모든 행을 검색합니다.
+*/
+SELECT e.last_name, e.department_id, d.department_name
+FROM employees e
+FULL OUTER JOIN departments d
+ON e.department_id = d.department_id;
+
+/*
+Cartesian Product
+    조인 조건이 잘못되거나 완전히 생략된 경우 결과가 모든 행의 조합이 표시되는
+    Cartesian Product로 나타냅니다.
+*/
+
+/*
+Cross Join
+    두 테이블의 교차 곱을 생성합니다.
+*/
+SELECT last_name, department_name
+FROM employees
+CROSS JOIN departments;
+
+
+
